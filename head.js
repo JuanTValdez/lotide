9f0bace9d4518f0888920190840260e39d6bdd3b
Juan <juantvaldez85.@gmail.com>
2021-08-04 05:36:20 +0000

    completed implement head challenge

---
+++ head.js
@@ -0,0 +1,20 @@
+const head = function (array) {
+  const firstElement = array[0];
+
+  return firstElement;
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(
+      "👽👽👽 Assertion Passed: " + `${actual}` + " === " + `${expected}`
+    );
+  } else if (actual !== expected) {
+    console.log(
+      "👽👽👽 Assertion Failed: " + `${actual}` + " !== " + `${expected}`
+    );
+  }
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
