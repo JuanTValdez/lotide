8f2170522452734e5b78e90ff83af163ffca62e3
Juan <juantvaldez85.@gmail.com>
2021-08-11 22:52:56 +0000

    re-added assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,36 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(
+      "👽👽👽 Assertion Passed: " + `${actual}` + " === " + `${expected}`
+    );
+  } else if (actual !== expected) {
+    console.log(
+      "👽👽👽 Assertion Failed: " + `${actual}` + " !== " + `${expected}`
+    );
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+
+assertEqual(1, 1);
+
+/* HERE */
+
+// const sum = function (a, b) {
+//   return a + b;
+// };
+
+// console.assert(sum(1, 2) === 3);
+// console.assert(sum(1, 20) === 3);
+// /* TO  HERE */
+
+// /* HERE */
+
+// const sumBuggy = function (a, b) {
+//   return a * b;
+// };
+
+// console.assert(sumBuggy(1, 2) === 3);
+
+/* TO HERE */
